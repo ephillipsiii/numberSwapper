40307999e1666c886f80863210e04763ecf8df1e
Lappy Mc Lap Face <ephillipsiii>
2018-05-29 18:39:49 -0600

    rough pseudo code

---
+++ swapper.js
@@ -0,0 +1,3 @@
+// I want to create a program that loops through an array of numbers (1-100)
+// Each even number will be replaced with "foo" and each odd number will be replaced with "bar"
+// I should stillbe able to call either the numbers or the replacements, giving various console logs.
